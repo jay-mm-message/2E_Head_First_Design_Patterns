94892e0a0aad5e70933e8e8b255c6e6747fcb899
jay-mm-message <jay.mm.message@gmail.com>
2024-07-11 12:34:29 +0800

    refactor

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/iterator/dinermerger/PancakeHouseMenu.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/iterator/dinermerger/PancakeHouseMenu.java
@@ -5,29 +5,11 @@ import java.util.List;
 
 public class PancakeHouseMenu implements Menu {
 	List<MenuItem> menuItems;
- 
+	
 	public PancakeHouseMenu() {
 		menuItems = new ArrayList<MenuItem>();
-    
-		addItem("K&B's Pancake Breakfast", 
-			"Pancakes with scrambled eggs and toast", 
-			true,
-			2.99);
- 
-		addItem("Regular Pancake Breakfast", 
-			"Pancakes with fried eggs, sausage", 
-			false,
-			2.99);
- 
-		addItem("Blueberry Pancakes",
-			"Pancakes made with fresh blueberries",
-			true,
-			3.49);
- 
-		addItem("Waffles",
-			"Waffles with your choice of blueberries or strawberries",
-			true,
-			3.59);
+		
+		createMenuItemsList();
 	}
 
 	public void addItem(String name, String description,
@@ -50,4 +32,25 @@ public class PancakeHouseMenu implements Menu {
 	}
 
 	// other menu methods here
+	public void createMenuItemsList() {
+		addItem("K&B's Pancake Breakfast", 
+			"Pancakes with scrambled eggs and toast", 
+			true,
+			2.99);
+ 
+		addItem("Regular Pancake Breakfast", 
+			"Pancakes with fried eggs, sausage", 
+			false,
+			2.99);
+ 
+		addItem("Blueberry Pancakes",
+			"Pancakes made with fresh blueberries",
+			true,
+			3.49);
+ 
+		addItem("Waffles",
+			"Waffles with your choice of blueberries or strawberries",
+			true,
+			3.59);
+	}
 }
