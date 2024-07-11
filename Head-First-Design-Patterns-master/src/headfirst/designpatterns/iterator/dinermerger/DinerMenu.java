3b6e488f10412c7d06da9d2c7835d08448024fd6
jay-mm-message <jay.mm.message@gmail.com>
2024-07-11 12:38:12 +0800

    refactor

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/iterator/dinermerger/DinerMenu.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/iterator/dinermerger/DinerMenu.java
@@ -8,20 +8,7 @@ public class DinerMenu implements Menu {
 	public DinerMenu() {
 		menuItems = new MenuItem[MAX_ITEMS];
  
-		addItem("Vegetarian BLT",
-			"(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
-		addItem("BLT",
-			"Bacon with lettuce & tomato on whole wheat", false, 2.99);
-		addItem("Soup of the day",
-			"Soup of the day, with a side of potato salad", false, 3.29);
-		addItem("Hotdog",
-			"A hot dog, with sauerkraut, relish, onions, topped with cheese",
-			false, 3.05);
-		addItem("Steamed Veggies and Brown Rice",
-			"Steamed vegetables over brown rice", true, 3.99);
-		addItem("Pasta",
-			"Spaghetti with Marinara Sauce, and a slice of sourdough bread",
-			true, 3.89);
+		createMenuItemsList();
 	}
   
 	public void addItem(String name, String description, 
@@ -48,4 +35,20 @@ public class DinerMenu implements Menu {
 	}
  
 	// other menu methods here
+	public void createMenuItemsList() {
+		addItem("Vegetarian BLT",
+		"(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
+		addItem("BLT",
+			"Bacon with lettuce & tomato on whole wheat", false, 2.99);
+		addItem("Soup of the day",
+			"Soup of the day, with a side of potato salad", false, 3.29);
+		addItem("Hotdog",
+			"A hot dog, with sauerkraut, relish, onions, topped with cheese",
+			false, 3.05);
+		addItem("Steamed Veggies and Brown Rice",
+			"Steamed vegetables over brown rice", true, 3.99);
+		addItem("Pasta",
+			"Spaghetti with Marinara Sauce, and a slice of sourdough bread",
+			true, 3.89);
+	}
 }
