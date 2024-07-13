e0d47e64b83928143de390afd3da8623330982af
jay-mm-message <jay.mm.message@gmail.com>
2024-07-13 10:50:01 +0800

    refactor SoldState

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumballstate/SoldState.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumballstate/SoldState.java
@@ -9,15 +9,15 @@ public class SoldState implements State {
     }
        
 	public void insertQuarter() {
-		System.out.println("Please wait, we're already giving you a gumball");
+		execute("Please wait, we're already giving you a gumball");
 	}
  
 	public void ejectQuarter() {
-		System.out.println("Sorry, you already turned the crank");
+		execute("Sorry, you already turned the crank");
 	}
  
 	public void turnCrank() {
-		System.out.println("Turning twice doesn't get you another gumball!");
+		execute("Turning twice doesn't get you another gumball!");
 	}
  
 	public void dispense() {
@@ -25,7 +25,7 @@ public class SoldState implements State {
 		if (gumballMachine.getCount() > 0) {
 			gumballMachine.setState(gumballMachine.getNoQuarterState());
 		} else {
-			System.out.println("Oops, out of gumballs!");
+			execute("Oops, out of gumballs!");
 			gumballMachine.setState(gumballMachine.getSoldOutState());
 		}
 	}
@@ -34,6 +34,10 @@ public class SoldState implements State {
  
 	public String toString() {
 		return "dispensing a gumball";
+	}
+	
+	public void execute(String msg) {
+		System.out.println(msg);
 	}
 }
 
