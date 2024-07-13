d232ca3c5ee95d1e77c7ad09e4a37fdf720bbcb1
jay-mm-message <jay.mm.message@gmail.com>
2024-07-13 09:58:54 +0800

    refactor dispense

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumball/GumballMachine.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumball/GumballMachine.java
@@ -83,21 +83,28 @@ public class GumballMachine {
 	}
  
 	private void dispense() {
-		if (state == SOLD) {
-			System.out.println("A gumball comes rolling out the slot");
-			count = count - 1;
-			if (count == 0) {
-				System.out.println("Oops, out of gumballs!");
-				state = SOLD_OUT;
-			} else {
-				state = NO_QUARTER;
-			}
-		} else if (state == NO_QUARTER) {
-			System.out.println("You need to pay first");
-		} else if (state == SOLD_OUT) {
-			System.out.println("No gumball dispensed");
-		} else if (state == HAS_QUARTER) {
-			System.out.println("No gumball dispensed");
+		switch (state) {
+			case SOLD:
+				execute("A gumball comes rolling out the slot");
+				count = count - 1;
+				if (count == 0) {
+					execute("Oops, out of gumballs!");
+					state = SOLD_OUT;
+				} else {
+					state = NO_QUARTER;
+				}
+				break;
+			case NO_QUARTER:
+				execute("You need to pay first");
+				break;
+			case SOLD_OUT:
+				execute("No gumball dispensed");
+				break;
+			case HAS_QUARTER:
+				execute("No gumball dispensed");
+				break;
+			default:
+				break;
 		}
 	}
  
