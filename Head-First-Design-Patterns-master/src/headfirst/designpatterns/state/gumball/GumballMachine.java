3a7897ab152fbf9462baaf1a09975dbfcda4ae8e
jay-mm-message <jay.mm.message@gmail.com>
2024-07-13 09:55:27 +0800

    refactor ejectQuarter

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumball/GumballMachine.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumball/GumballMachine.java
@@ -42,15 +42,22 @@ public class GumballMachine {
 	}
 
 	public void ejectQuarter() {
-		if (state == HAS_QUARTER) {
-			System.out.println("Quarter returned");
-			state = NO_QUARTER;
-		} else if (state == NO_QUARTER) {
-			System.out.println("You haven't inserted a quarter");
-		} else if (state == SOLD) {
-			System.out.println("Sorry, you already turned the crank");
-		} else if (state == SOLD_OUT) {
-        	System.out.println("You can't eject, you haven't inserted a quarter yet");
+		switch (state) {
+			case HAS_QUARTER:
+				execute("Quarter returned");
+				state = NO_QUARTER;
+				break; 
+			case NO_QUARTER:
+				execute("You haven't inserted a quarter");
+				break; 
+			case SOLD:
+				execute("Sorry, you already turned the crank");
+				break; 
+			case SOLD_OUT:
+				execute("You can't eject, you haven't inserted a quarter yet");
+				break;
+			default:
+				break;
 		}
 	}
  
