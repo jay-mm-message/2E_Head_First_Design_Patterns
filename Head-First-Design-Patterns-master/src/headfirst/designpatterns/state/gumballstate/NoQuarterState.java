52f4df23c597950786cc17d0d4b4a7331f635a56
jay-mm-message <jay.mm.message@gmail.com>
2024-07-13 10:52:07 +0800

    refactor NoQuarterState

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumballstate/NoQuarterState.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/state/gumballstate/NoQuarterState.java
@@ -8,25 +8,29 @@ public class NoQuarterState implements State {
     }
  
 	public void insertQuarter() {
-		System.out.println("You inserted a quarter");
+		execute("You inserted a quarter");
 		gumballMachine.setState(gumballMachine.getHasQuarterState());
 	}
  
 	public void ejectQuarter() {
-		System.out.println("You haven't inserted a quarter");
+		execute("You haven't inserted a quarter");
 	}
  
 	public void turnCrank() {
-		System.out.println("You turned, but there's no quarter");
+		execute("You turned, but there's no quarter");
 	 }
  
 	public void dispense() {
-		System.out.println("You need to pay first");
+		execute("You need to pay first");
 	} 
 	
 	public void refill() { }
  
 	public String toString() {
 		return "waiting for quarter";
+	}
+
+	public void execute(String msg) {
+		System.out.println(msg);
 	}
 }
