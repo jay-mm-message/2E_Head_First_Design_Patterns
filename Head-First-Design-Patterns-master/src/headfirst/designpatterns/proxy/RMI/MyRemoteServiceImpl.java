ec779b17bd9cb68e475b417662e4be5e878959b6
jay-mm-message <jay.mm.message@gmail.com>
2024-07-14 09:43:56 +0800

    refactor

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/proxy/RMI/MyRemoteServiceImpl.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/proxy/RMI/MyRemoteServiceImpl.java
@@ -3,7 +3,7 @@ package headfirst.designpatterns.proxy.RMI;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class MyRemoteServiceImpl extends UnicastRemoteObject implements MyRemoteService{
+public class MyRemoteServiceImpl extends UnicastRemoteObject implements MyRemoteService {
 
     public String sayHi()throws RemoteException {
        return "!!!! Server Says, 'Hey'";
