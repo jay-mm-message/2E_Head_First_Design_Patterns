bb1fcd15281c8a3e6449591d4beb99866c86cb03
jay-mm-message <jay.mm.message@gmail.com>
2024-07-14 10:20:41 +0800

    Add find pid

--- Head-First-Design-Patterns-master/src/run_service.sh
+++ Head-First-Design-Patterns-master/src/run_service.sh
@@ -1,2 +1,3 @@
+# netstat -van | grep 1099 # find service id
 clear
 java -cp /Users/chun-chieh/Downloads/github/2E_Head_First_Design_Patterns/Head-First-Design-Patterns-master/src/headfirst/designpatterns/proxy/RMI/class/ headfirst.designpatterns.proxy.RMI.Server
